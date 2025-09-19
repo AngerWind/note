59c08ed4f3086c53136bd56b9711a68a172fd30f
shenqitai ys49482 (CW <YS.shenqitai@h3c.com>
2025-09-19 13:16:14 +0800

    2025/09/19 周五

--- gitcm_github.bat
+++ gitcm_github.bat
@@ -1,7 +1,17 @@
-﻿cd /d "%~dp0"
-git add .
-git commit -m "%date%"
-git pull --rebase 
-git push github
-pause
+﻿
+REM 切换到当前脚本所在目录
+cd /d "%~dp0"
 
+REM 添加改动
+git add .
+
+REM 提交，提交信息是当前日期
+git commit -m "%date%"
+
+REM 拉取远程分支并 rebase
+git pull --rebase
+
+REM 推送到 github 远程
+git push github
+
+pause
